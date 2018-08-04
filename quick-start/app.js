800a83a098448f3982c9f1b90a140202593f0608
JL <jliu20@syr.edu>
2018-08-04 15:06:31 -0700

    fix

--- quick-start/app.js
+++ quick-start/app.js
@@ -120,7 +120,7 @@ function handleMessage(sender_psid, received_message) {
       }
       callSendAPI(sender_psid, response);
     } else {
-      address = match[1];
+      let address = match[1];
       console.log("Address: " + address);
       response = {
         "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
